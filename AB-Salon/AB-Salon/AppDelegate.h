54d91fbc6258466af9b4465b965da556b43e5d61
Hanny T <hanny@citrusbits.com>
2016-06-06 10:50:48 +0500

    Created AB Salon iOS project with Objective-C language

---
+++ AB-Salon/AB-Salon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AB-Salon
+//
+//  Created by zaktech on 06/06/2016.
+//  Copyright © 2016 Citrusbits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AB-Salon/AB-Salon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AB-Salon
+//
+//  Created by zaktech on 06/06/2016.
+//  Copyright © 2016 Citrusbits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
